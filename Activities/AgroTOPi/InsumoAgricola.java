adf12f6b74d942fb73779ee1a3abdec1407a6ec7
Eduardo Giehl <BaldinhoSSlive@outlook.com>
2024-08-06 10:40:38 -0300

    Update InsumoAgricola.java

---
+++ Activities/AgroTOPi/InsumoAgricola.java
@@ -0,0 +1,51 @@
+package AgroTOPi;
+
+public class InsumoAgricola {
+    private String nome;
+    private double precoPorUnidade;
+    private int estoque;
+
+    // Construtor
+    public InsumoAgricola(String nome, double precoPorUnidade, int estoque) {
+        this.nome = nome;
+        this.precoPorUnidade = precoPorUnidade;
+        this.estoque = estoque;
+    }
+
+    // Métodos para acessar e modificar os atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrecoPorUnidade() {
+        return precoPorUnidade;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setPrecoPorUnidade(double precoPorUnidade) {
+        this.precoPorUnidade = precoPorUnidade;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    // Método para atualizar o estoque
+    public void atualizarEstoque(int quantidade) {
+        this.estoque += quantidade;
+        if (this.estoque < 0) {
+            this.estoque = 0;
+        }
+    }
+
+    // Método para exibir as informações do insumo
+    public void exibirInformacoes() {
+        System.out.println("Insumo Agrícola: " + nome);
+        System.out.println("Preço por Unidade: R$" + precoPorUnidade);
+        System.out.println("Estoque: " + estoque + " unidades");
+    }
+}
+
